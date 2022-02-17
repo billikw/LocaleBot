47400dbe6439434f8999db364bfe0c73c126b151
billikw <66752314+billikw@users.noreply.github.com>
2022-02-17 23:28:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -42,12 +42,19 @@ bot.on("message", msg => {
     
             // NOTE - Currently there is no way to tell the bot to listen to a specific channel. So there is no code
             // to check for this. The work around is making all other channels private to the bot only!
-       if (msg.content.includes('https://ebay') || msg.content.includes('https://www.ebay') || msg.content.includes('http://ebay') || msg.content.includes('http://www.ebay')) {
+        if (msg.content.includes('https://ebay') || msg.content.includes('https://www.ebay') || msg.content.includes('http://ebay') || msg.content.includes('http://www.ebay')) {
             for (let i=0; i<ebaySuffixAndFlags.length; i++) {
                 if (msg.content.includes(ebaySuffixAndFlags[i][0])) {
                         msg.react(ebaySuffixAndFlags[i][1])
                         msg.react(leftArrow)
                         break
+                }
+            }
+        } else {
+            for (let i=0; i<otherUrls.length; i++){
+                if (msg.content.includes(otherUrls[i])){
+                    msg.react(ebaySuffixAndFlags[13][1])
+                    msg.react(leftArrow)
                 }
             }
         }
